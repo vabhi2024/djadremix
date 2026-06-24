099e1b737450c3728b11af234acfef174da20b75
vabhi2024 <vabhi2024@gmail.com>
2026-06-24 19:40:47 +0530

    Add files via upload

---
+++ page/footer.js
@@ -0,0 +1,15 @@
+function openMenu() {
+  document.getElementById('navMenu').classList.add('show');
+}
+
+function closeMenu() {
+  document.getElementById('navMenu').classList.remove('show');
+}
+
+// Mobile Menu Toggle
+function openMenu() {
+  document.getElementById("mobileNav").style.display = "flex";
+}
+function closeMenu() {
+  document.getElementById("mobileNav").style.display = "none";
+}
